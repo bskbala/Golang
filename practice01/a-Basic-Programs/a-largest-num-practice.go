173167f1eecba860d2924e6e1637be60a924ac09
bskbala <bskbala410@gmail.com>
2021-04-19 02:18:54 -0500

    first commit

---
+++ practice01/a-Basic-Programs/a-largest-num-practice.go
@@ -0,0 +1,26 @@
+package main
+
+import ( "fmt"
+)
+
+func main (){
+
+	fmt.Println("Enter first Number:")
+	var first int
+	fmt.Scanf("%d" ,&first)
+	fmt.Println("Enter second  Number:")
+	var second int
+	fmt.Scanf("%d" ,&second)
+	fmt.Println("Enter third  Number:")
+	var third int
+	fmt.Scanf("%d" ,&third)
+
+	if (first >=second && first >=third){
+		fmt.Println("first is the largest Numeber")
+	}else if (second >=first && second >=third){
+		fmt.Println("second is the largest Numeber")
+	}else if (third >=first && third >=second){
+		fmt.Println("third is the largest Numeber")
+	}
+
+}
